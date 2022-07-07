4c7c71420f8ec9973f12d35a10fc13c877ca42af
teddylear <teddylear@gmail.com>
2022-07-06 20:48:11 -0400

    Adding basic configuration to have open telemetry on endpoint

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Django open telemetry playground
 
 Sample Django app with open telemetry testing some concepts
+
+## Running test server
+
+```zsh
+pipenv sync --dev
+pipenv shell
+cd demo_project
+DJANGO_SETTINGS_MODULE=demo_project.settings opentelemetry-instrument python manage.py runserver --noreload
+```
--- demo_project/HelloWorldApp/views.py
+++ demo_project/HelloWorldApp/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+trace.set_tracer_provider(TracerProvider())
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
+
 
 def helloWorld(request):
     return HttpResponse("Hello World")
